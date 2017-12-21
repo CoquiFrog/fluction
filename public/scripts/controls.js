6e0876e1b7bc8f800be03608785cb8ad4e235bfe
CoquiFrog <polzer81@hotmail.com>
2017-12-21 15:07:04 -0500

    updated functionality on the jukebox

--- public/scripts/controls.js
+++ public/scripts/controls.js
@@ -1,5 +1,4 @@
-// window.alert('poopy pants22');
-console.log('Hello hello hello');
+
 var mytrack = document.getElementById('mytrack');
 var playButton = document.getElementById('playButton');
 var muteButton = document.getElementById('muteButton');
@@ -7,16 +6,24 @@ var muteButton = document.getElementById('muteButton');
 var duration = document.getElementById('fullDuration');
 var currentTime = document.getElementById('currentTime');
 
+
+// needs to be set to the css px number in index.css
+var barSize = 640;
+
+var bar = document.getElementById('defaultBar');
+var progressBar = document.getElementById('progressBar');
+
 duration.innerHTML = mytrack.duration
 
-playButton.addEventListener('click',playOrPause,false);
-muteButton.addEventListener('click',muteOrUnmute,false);
+playButton.addEventListener('click', playOrPause, false);
+muteButton.addEventListener('click', muteOrUnmute, false);
+bar.addEventListener('click', clickedBar, false);
 
 // This is an offered solution to the NaN error, but it is using an event listener
 mytrack.addEventListener("loadedmetadata", function () {
     //you can display the duration now
     var minutes = parseInt(mytrack.duration / 60);
-    var seconds = parseInt(mytrack.duration % 60);
+    var seconds = pad(parseInt(mytrack.duration % 60));
     duration.innerHTML = minutes + ':' + seconds;
 });
 // ends here
@@ -32,43 +39,57 @@ mytrack.addEventListener("loadedmetadata", function () {
 
 
 
-function playOrPause(){
-    if(!mytrack.paused && !mytrack.ended){
+function playOrPause() {
+    if (!mytrack.paused && !mytrack.ended) {
         mytrack.pause();
-        console.log("Function is working!");
         playButton.style.backgroundImage = 'url("/publicimages/play6.png")';
         window.clearInterval(updateTime);
     }
-    else{
+    else {
         mytrack.play();
-        console.log("Function is working2222!");
         playButton.style.backgroundImage = 'url("/publicimages/pause.png")';
-        updateTime = setInterval(update,500);
+        updateTime = setInterval(update, 500);
     }
 }
 
-function muteOrUnmute(){
-    if(mytrack.muted == true){
+function muteOrUnmute() {
+    if (mytrack.muted == true) {
         mytrack.muted = false;
-        console.log("working here");
         muteButton.style.backgroundImage = 'url("/publicimages/mute2.jpg")';
 
     }
-    else{
+    else {
         mytrack.muted = true;
-        console.log("working here222");
         muteButton.style.backgroundImage = 'url("/publicimages/unmute.png")';
     }
 }
 
-function update(){
-    if(!mytrack.ended){
-        var playedMinutes = parseInt(mytrack.currentTime/60);
-        var playedSeconds = parseInt(mytrack.currentTime%60);
+function update() {
+    if (!mytrack.ended) {
+        var playedMinutes = parseInt(mytrack.currentTime / 60);
+        var playedSeconds = parseInt(mytrack.currentTime % 60);
         currentTime.innerHTML = playedMinutes + ':' + playedSeconds;
+        var size = parseInt(mytrack.currentTime * barSize / mytrack.duration);
+        progressBar.style.width = size + "px";
     }
-    else{
+    else {
         currentTime.innerHTML = "0.00";
         playButton.style.backgroundImage = 'url("/publicimages/play6.png")';
+        progressBar.style.width = "0px";
+        window.clearInterval(updateTime);
     }
+}
+
+function clickedBar(e) {
+    if (!mytrack.ended) {
+        var mouseX = e.pageX - bar.offsetLeft;
+        var newtime = mouseX * mytrack.duration / barSize;
+
+        mytrack.currentTime = newtime;
+        progressBar.style.width = mouseX + 'px';
+    }
+}
+
+function pad(d){
+    return (d < 10) ? '0' + d.toString() : d.toString();
 }
