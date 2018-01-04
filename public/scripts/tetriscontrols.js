3850bc46b6e389e2fc72565a12ecd13a90fd59f0
CoquiFrog <polzer81@hotmail.com>
2018-01-04 18:46:31 -0500

    Added timed block dropping and basic key movement

--- public/scripts/tetriscontrols.js
+++ public/scripts/tetriscontrols.js
@@ -3,14 +3,21 @@ const context = canvas.getContext('2d');
 
 context.scale(20,20);
 
-context.fillStyle = '#b4da55';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
+
+function draw(){
+
+    context.fillStyle = '#b4da55';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawMatrix(player.matrix, player.pos);
+}
 
 function drawMatrix(matrix, offset){
 matrix.forEach((row, y) => {
@@ -23,10 +30,37 @@ matrix.forEach((row, y) => {
     });
 });
 }
+let dropCounter = 0;
+let dropInterval = 1000;
+
+
+let lastTime = 0;
+function update(time = 0){
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    // console.log(time);
+    draw();
+    requestAnimationFrame(update);
+
+}
 
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix,
 }
 
-drawMatrix(player.matrix, player.pos);
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39){
+        player.pos.x++;
+    }
+});
+
+update();
