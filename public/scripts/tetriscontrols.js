cfc644b34103d298a5a242d1d22323a57d728920
CoquiFrog <polzer81@hotmail.com>
2018-01-06 23:32:02 -0500

    added pieces

--- public/scripts/tetriscontrols.js
+++ public/scripts/tetriscontrols.js
@@ -13,11 +13,11 @@ const matrix = [
 
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
-    for (let y = 0; y < m.length; ++y){
-        for (let x = 0; x < m[y].length; ++x){
-            if (m[y][x] !== 0 && 
-            (arena[y + o.y] &&
-            arena[y + o.y][x + o.x]) !== 0) {
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                    arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
@@ -25,9 +25,9 @@ function collide(arena, player) {
     return false;
 }
 
-function createMatrix(w,h){
+function createMatrix(w, h) {
     const matrix = [];
-    while (h--){
+    while (h--) {
         matrix.push(new Array(w).fill(0));
     }
     return matrix;
@@ -37,12 +37,61 @@ const arena = createMatrix(12, 20);
 console.log(arena); console.table(arena);
 
 
+function createPiece(type) {
+    if (type === 'T') {
+        return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+    } else if (type === 'L') {
+        return [
+
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1],
+        ];
+
+    } else if (type === 'O'){
+        return[
+            [1, 1],
+            [1, 1],
+        ];
+    } else if (type === 'J'){
+        return[
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+        ];
+    } else if (type === 'I'){
+        return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ]
+    } else if (type === 'S'){
+        return[
+                [0, 1, 1],
+                [1, 1, 0],
+                [0, 0, 0],
+            ];
+        
+    } else if (type === 'Z'){
+        return[
+                [1, 1, 0],
+                [0, 1, 1],
+                [0, 0, 0],
+            ];
+}
+}
+
 function draw() {
 
     context.fillStyle = '#b4da55';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawMatrix(arena, {x: 0, y: 0})
+    drawMatrix(arena, { x: 0, y: 0 })
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -58,10 +107,10 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-function merge(arena, player){
+function merge(arena, player) {
     player.matrix.forEach((row, y) => {
         row.forEach((value, x) => {
-            if (value !== 0){
+            if (value !== 0) {
                 arena[y + player.pos.y][x + player.pos.x] = value;
             }
         })
@@ -71,30 +120,30 @@ function merge(arena, player){
 
 function playerDrop() {
     player.pos.y++;
-if (collide(arena, player)) {
-    player.pos.y--;
-    merge(arena, player);
-    player.pos.y = 0;
-}
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
 
     dropCounter = 0;
 }
 
-function playerMove(dir){
+function playerMove(dir) {
     player.pos.x += dir;
-    if (collide (arena, player)){
+    if (collide(arena, player)) {
         player.pos.x -= dir;
     }
 }
 
-function playerRotate(dir){
+function playerRotate(dir) {
     const pos = player.pos.x;
     let offset = 1;
     rotate(player.matrix, dir);
-    while (collide(arena, player)){
+    while (collide(arena, player)) {
         player.pos.x += offset;
         offset = -(offset + (offset > 0 ? 1 : -1))
-        if (offset > player.matrix[0].length){
+        if (offset > player.matrix[0].length) {
             rotate(player.matrix, -dir);
             player.pos.x = pos;
             return;
@@ -103,23 +152,23 @@ function playerRotate(dir){
 }
 
 function rotate(matrix, dir) {
-    for(let y = 0; y < matrix.length; ++y){
-        for (let x = 0; x < y; ++x){
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
             [
                 matrix[x][y],
                 matrix[y][x],
             ] = [
-                matrix[y][x],
-                matrix[x][y],
-            ];
+                    matrix[y][x],
+                    matrix[x][y],
+                ];
 
         }
     }
-if (dir > 0) {
-    matrix.forEach(row => row.reverse());
-} else {
-    matrix.reverse();
-}
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
 }
 
 
@@ -151,7 +200,7 @@ function update(time = 0) {
 
 const player = {
     pos: { x: 5, y: 5 },
-    matrix: matrix,
+    matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
