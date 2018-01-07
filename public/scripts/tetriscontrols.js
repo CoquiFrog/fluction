7fd226b884e19118423f45aeda89bff385d6a705
CoquiFrog <polzer81@hotmail.com>
2018-01-06 19:42:33 -0500

    added piece rotation and fixed drop key

--- public/scripts/tetriscontrols.js
+++ public/scripts/tetriscontrols.js
@@ -80,12 +80,61 @@ if (collide(arena, player)) {
     dropCounter = 0;
 }
 
+function playerMove(dir){
+    player.pos.x += dir;
+    if (collide (arena, player)){
+        player.pos.x -= dir;
+    }
+}
+
+function playerRotate(dir){
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)){
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1))
+        if (offset > player.matrix[0].length){
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for(let y = 0; y < matrix.length; ++y){
+        for (let x = 0; x < y; ++x){
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+
+        }
+    }
+if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+} else {
+    matrix.reverse();
+}
+}
+
+
+
+
+
+
+
 
 let dropCounter = 0;
 let dropInterval = 1000;
 
 
 let lastTime = 0;
+
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
@@ -107,11 +156,15 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1);
     } else if (event.keyCode === 39) {
-        player.pos.x++;
-    } else if (event.keycode === 40) {
+        playerMove(1);
+    } else if (event.keyCode === 83) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
 });
 
